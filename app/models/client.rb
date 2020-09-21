4d1aeef3f8ed96fa40399011e08c876c57c4ca6c
Grzegorz Wilusz <wilusz.gregory@gmail.com>
2020-09-21 13:44:17 +0200

    Utilize STI to Users & create Admin, Client and Salesman models

---
+++ app/models/client.rb
@@ -0,0 +1,3 @@
+class Client < User
+  has_many :offers
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_102944) do
+ActiveRecord::Schema.define(version: 2020_09_21_112402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_102944) do
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "type", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
