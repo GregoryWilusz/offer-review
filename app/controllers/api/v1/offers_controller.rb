774d89acf8b9304fd66714bafe794d57932df46d
Grzegorz Wilusz <wilusz.gregory@gmail.com>
2020-09-21 12:44:40 +0200

    Create an Offer

--- app/controllers/api/v1/offers_controller.rb
+++ app/controllers/api/v1/offers_controller.rb
@@ -2,6 +2,12 @@ module Api
   module V1
     class OffersController < ApplicationController
       def create
+        param! :user_id, String, required: true
+        param! :quantity, Integer, required: true
+
+        user = User.find(params[:user_id])
+        Offer.create!(user_id: user.id, quantity: params[:quantity])
+
         render json: { success: true }, status: 201
       end
     end
